a41fe43438b14e76d3d67e5547a28ef12556511f
Thiago Lagden <lagden@gmail.com>
2016-09-11 22:41:53 -0300

    added license and accept only local files

--- lagden-include.php
+++ lagden-include.php
@@ -3,7 +3,7 @@
 Plugin Name: Lagden Include
 Plugin URI:  http://lagden.in
 Description: Short code to inject content into page
-Version:     1.0.0
+Version:     2.0.0
 Author:      Thiago Lagden
 Author URI:  http://lagden.in
 License:     MIT
@@ -11,11 +11,9 @@ License URI: https://opensource.org/licenses/MIT
 */
 
 function lagden_in_shortcode($atts) {
-	$regex_url = '/http(s)?:\/\/[(www\.)?a-zA-Z0-9\@\:\%\.\_\+\~\#\=]{2,256}\.[a-z]{2,6}\b([\-a-zA-Z0-9\@\:\%\_\+\.\~\#\?\&\/\/\=]*)/i';
 	$_atts = shortcode_atts(['src' => false], $atts);
 	if ($_atts['src']) {
-		$abs_path = preg_match($regex_url, $_atts['src']) ? '' : ABSPATH;
-		$content = @file_get_contents("{$abs_path}{$_atts['src']}");
+		$content = @file_get_contents(ABSPATH . "{$_atts['src']}");
 		if ($content === false) {
 			return "File not found: {$_atts['src']}";
 		}
